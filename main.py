27a069ec3a1d9ad06058accd76b4f4c40360d76b
rgsk <guptarahul70322@gmail.com>
2023-04-28 12:41:06 +0530

    predicting from imported model and data rather than ipynb file

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
-from ipynb.fs.full.base import get_companies, get_models_for_company, get_fuel_types_for_model, predict_price
 from fastapi import FastAPI, HTTPException
+import pandas as pd
+import pickle
 
 app = FastAPI()
+
+lr_model = pickle.load(open("LinearRegressionModel.pkl", 'rb'))
+
+
+def predict_price(company: str, model: str, year_of_purchase: int, fuel_type: str, kms_driven: int):
+    return lr_model.predict(pd.DataFrame([[company, model, year_of_purchase, fuel_type, kms_driven]], columns=['company', 'name', 'year', 'fuel_type', 'kms_driven']))[0]
+
+
+def get_models_for_company(company: str):
+    return sorted(cars[cars['company'] == company]['name'].unique())
+
+
+def get_fuel_types_for_model(model: str):
+    return sorted(cars[cars['name'] == model]['fuel_type'].unique())
+
+
+cars = pd.read_csv("cleaned_car.csv")
+
+
+def get_companies():
+    return sorted(cars['company'].unique())
 
 
 @app.get('/')
